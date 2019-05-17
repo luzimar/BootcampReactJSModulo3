cc81ea989436766fe2bc2b66462d25cf52bc183f
luzimar <lzm.spfc@hotmail.com>
2019-05-16 22:25:18 -0300

    Implementado configuração para uso do reactotron em ambiente de desenvolvimento

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from "react";
+import "./config/reactotron";
 import { Provider } from "react-redux";
 import store from "./store/index";
 import TodoList from "./TodoList";
+
+console.tron.log("Testando");
 
 const App = () => (
   <Provider store={store}>
